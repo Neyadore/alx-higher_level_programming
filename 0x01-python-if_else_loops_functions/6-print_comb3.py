751ce809351c1d840e3f6bbec505b41b14dd3483
Neyadore <n.metsing00@gmail.com>
2023-01-10 15:22:51 -0800

    added whitespace for pep8

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for a in range(0,8):
+for a in range(0, 8):
     b = a + 1
-    for b in range(b,10):
+    for b in range(b, 10):
         print("{:d}{:d}".format(a, b), end=", ")
 print("89")
